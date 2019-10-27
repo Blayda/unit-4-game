7b1f2d24c41512c15f7f6e05eba8ecf08866a0cb
blayda <zacblayda@gmail.com>
2019-10-26 21:11:37 -0700

    adding images

---
+++ assets/javascript/game.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+  var x = $("#number");
+  var y = $("#guessed");
+  var z = $("#wins");
+  var a = $("#lose");
+
+  var number = Math.floor(Math.random() * (120 - 19) + 19);
+  var counter = 0;
+
+  var red = Math.floor(Math.random() * 11) + 1;
+  var blue = Math.floor(Math.random() * 11) + 1;
+  var green = Math.floor(Math.random() * 11) + 1;
+  var yellow = Math.floor(Math.random() * 11) + 1;
+  var wins = 0;
+  var losses = 0;
+
+  x.text(number);
+  y.text(counter);
+  z.text(wins);
+  a.text(losses);
+
+  function restart() {
+    console.log(y.val());
+    number = Math.floor(Math.random() * (120 - 19) + 19);
+    red = Math.floor(Math.random() * 11) + 1;
+    blue = Math.floor(Math.random() * 11) + 1;
+    green = Math.floor(Math.random() * 11) + 1;
+    yellow = Math.floor(Math.random() * 11) + 1;
+
+    counter = 0;
+    x.text(number);
+    y.text(counter);
+  }
+
+  function winGame() {
+    alert("You nailed it!");
+    wins++;
+    z.text(wins);
+  }
+
+  function loseGame() {
+    alert("Bummer, try again!");
+    losses++;
+    a.text(losses);
+  }
+
+  $("#red").on("click", function() {
+    if (counter != number) {
+      counter += red;
+      y.text(counter);
+      if (counter > number) {
+        loseGame();
+        restart();
+      } else if (counter == number) {
+        winGame();
+        restart();
+      }
+    }
+  });
+
+  $("#blue").on("click", function() {
+    if (counter != number) {
+      counter += blue;
+      y.text(counter);
+      if (counter > number) {
+        loseGame();
+        restart();
+      } else if (counter == number) {
+        winGame();
+        restart();
+      }
+    }
+  });
+
+  $("#green").on("click", function() {
+    if (counter != number) {
+      counter += green;
+      y.text(counter);
+      if (counter > number) {
+        loseGame();
+        restart();
+      } else if (counter == number) {
+        winGame();
+        restart();
+      }
+    }
+  });
+
+  $("#yellow").on("click", function() {
+    if (counter != number) {
+      counter += yellow;
+      y.text(counter);
+      if (counter > number) {
+        loseGame();
+        restart();
+      } else if (counter == number) {
+        winGame();
+        restart();
+      }
+    }
+  });
+});
